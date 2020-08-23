31f6b095389c6202d6338614ea2665ad5e536b6b
yamachoo <yamachoo567@gmail.com>
2020-08-23 22:05:03 +0900

    add api

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const { ENDPOINT_VERSION } = require('./constants/routes')
 
-const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/api/'.concat(ENDPOINT_VERSION, '/users'), usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
